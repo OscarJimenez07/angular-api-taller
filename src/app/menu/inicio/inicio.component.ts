b66e9b7f9a656a4f191ec4afdb813944d99d6de3
oscarjimenez07 <ojavierjimenez@uts.edu.co>
2024-04-21 16:19:10 -0500

    22042024

--- src/app/menu/inicio/inicio.component.ts
+++ src/app/menu/inicio/inicio.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../service/api.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent implements OnInit {
-
-  data: any[] = [];
-
-  constructor(private apiService: ApiService) {}
-
-  ngOnInit(): void {
-    this.setData();
-  }
-
-  setData() {
-    this.apiService.getData().subscribe( data => {
-      this.data = data;
-    })
-  }
-
+export class InicioComponent {
 
 }
--- src/app/menu/publicaciones/publicaciones.component.ts
+++ src/app/menu/publicaciones/publicaciones.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { ApiService } from '../../service/api.service';
 
 @Component({
   selector: 'app-publicaciones',
   templateUrl: './publicaciones.component.html',
   styleUrl: './publicaciones.component.css'
 })
-export class PublicacionesComponent {
+
+
+export class PublicacionesComponent implements OnInit {
+  data: any[] = []; // Arreglo para almacenar los datos obtenidos del servicio
+
+  constructor(private apiService: ApiService) {}
+
+  // Método del ciclo de vida OnInit, se ejecuta al inicializar el componente
+  ngOnInit(): void {
+    this.setData(); 
+  }
+  // Método personalizado para obtener y asignar datos desde el servicio
+  setData() {
+    // Llama al método getData del servicio ApiService para obtener los datos
+    this.apiService.getData().subscribe( data => {
+      this.data = data; // Asigna los datos obtenidos al arreglo 'data'
+    });
+  }
+
 
 }
